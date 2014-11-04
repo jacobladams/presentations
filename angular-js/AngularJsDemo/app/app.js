b3e52f3eb99998e6c733af83c9d8bd86b4938a98
Jacob Adams <jacobladams@gmail.com>
2014-11-03 22:02:36 -0600

    added more angular js demos

---
+++ angular-js/AngularJsDemo/app/app.js
@@ -0,0 +1,13 @@
+﻿angular.module('personnelDirectory', ['ngRoute']);
+
+angular.module('personnelDirectory').config('$routeProvider', function ($routeProvider) {
+	$routeProvider.
+		when('/personnel', {
+			controller: 'personnelListController',
+			templateUrl: 'personnel-list'
+		}).
+		when('/personnel/:id', {
+			controller: 'personnelController',
+			templateUrl: 'personnel'
+		});
+});
---
+++ angular-js/AngularJsDemo/app/controllers/personnelListController.js
@@ -0,0 +1,41 @@
+﻿
+angular.module('personnelDirectory').controller('personnelListController', ['$scope', function ($scope) {
+	var statuses = [{
+		id: 1,
+		name: 'Active'
+	}, {
+		id: 2,
+		name: 'Inactive'
+	}];
+
+	var personnelList = [{
+		id: 1,
+		firstName: 'Darth',
+		lastName: 'Vader',
+		title: 'Sith Lord',
+		image: 'someurltoanimage',
+		detailsUrl: 'somelinktodetails',
+		status: statuses[0]
+	}, {
+		id: 2,
+		firstName: 'Something',
+		lastName: 'Ozel',
+		title: 'Admiral',
+		image: 'someurltoanimage',
+		detailsUrl: 'somelinktodetails',
+		status: statuses[0]
+	}];
+
+	$scope.statuses = statuses;
+
+	$scope.personnelList = personnelList;
+
+	$scope.editPersonnel = function (personnel) {
+		personnel.isEditting = true;
+	}
+
+	$scope.savePersonnel = function (personnel) {
+		personnel.isEditting = false;
+	}
+
+}]);
